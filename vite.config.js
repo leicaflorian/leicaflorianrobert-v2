d4072883371614330bd560a64947c66de7e8d42f
leicaflorian <florian.leica@gmail.com>
2023-03-25 17:54:32 +0100

    Fix compilation bug

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,8 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/scss/app.scss', 'resources/js/app.js'],
-            refresh: true,
+            input: ['resources/js/app.js'],
+            refresh: true
         }),
     ],
 });
